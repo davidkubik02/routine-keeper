3d98173dd55853d0b9684e94d0310009711fb2b0
davidkubik02 <david.kubik02@gmail.com>
2023-03-19 19:49:32 +0100

    title change

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,6 @@ function App() {
   const basePath = "/reoutine-keeper"
   return (
     <BrowserRouter>
-    <div>sahoidsahoidsa</div>
-    <div>sahoidsahoidsa</div>
-    <div>sahoidsahoidsa</div>
-    <div>sahoidsahoidsa</div>
       {/* {user && <h1 onClick={logout}>{user.name}</h1>} */}
       <Routes>
         <Route path={basePath} element={<Home/>}/>
