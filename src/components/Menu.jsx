0086c4010ef68f775b065b042a809425268378e9
davidkubik02 <david.kubik02@gmail.com>
2023-03-19 19:36:12 +0100

    menu path fix

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 
 function Menu({active}) {
 
+
+  const basePath = "/reoutine-keeper"
   return (
     <nav className={`menu ${active?'menu-active':undefined}`}>
-        <Link className='menu-button' to='/'>Úkoly</Link>
-        <Link className='menu-button' to='/new'>Vytvořit nový</Link>
-        <Link className='menu-button' to='/login'>Přihlásit se</Link>
-        <Link className='menu-button' to='/register'>Zaregistrovat se</Link>
+        <Link className='menu-button' to={basePath}>Úkoly</Link>
+        <Link className='menu-button' to={basePath+'/new'}>Vytvořit nový</Link>
+        <Link className='menu-button' to={basePath+'/login'}>Přihlásit se</Link>
+        <Link className='menu-button' to={basePath+'/register'}>Zaregistrovat se</Link>
     </nav>
   )
 }
