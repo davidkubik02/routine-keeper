5785cb29668a11af7b0d331272b24cdd1118929e
davidkubik02 <david.kubik02@gmail.com>
2023-11-07 17:47:07 +0100

    menu fix

--- src/navigation/Menu.tsx
+++ src/navigation/Menu.tsx
@@ -1,37 +1,57 @@
-import React, { useContext } from "react";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
 import { routes } from "./routes";
-function Menu({ active }: { active: boolean }) {
+function Menu() {
   const { user, logout } = useContext(AuthContext);
+
+  const [menuActive, setMenuActive] = useState<boolean>(false);
+
+  const openMenu = () => {
+    setMenuActive(true);
+  };
+  const closeMenu = () => {
+    setMenuActive(false);
+  };
   return (
-    <nav className={`menu ${active ? "menu-active" : undefined}`}>
-      {user?.username ? (
-        <>
-          <h2>{user.username}</h2>
-          {routes.map((route) => {
-            if (route.isMenu) {
-              return (
-                <Link key={route.name} className="menu-button" to={route.path}>
-                  {route.name}
-                </Link>
-              );
-            } else return null;
-          })}
-          <button onClick={logout}>Log out</button>
-        </>
-      ) : (
-        <>
-          <Link className="menu-button" to="/login">
-            Přihlásit se
-          </Link>
-          <Link className="menu-button" to="/register">
-            Zaregistrovat se
-          </Link>
-        </>
-      )}
-    </nav>
+    <>
+      <i onClick={openMenu} className="menu-open-button fa-solid fa-bars" />
+      {menuActive && <div className="overlay" onClick={closeMenu} />}
+      <nav className={`menu ${menuActive && "menu-active"}`}>
+        <i
+          onClick={closeMenu}
+          className="menu-close-button fa-solid fa-xmark"
+        />
+        {user?.username ? (
+          <>
+            <h2>{user.username}</h2>
+            {routes.map((route) => {
+              if (route.isMenu) {
+                return (
+                  <Link
+                    key={route.name}
+                    className="menu-button"
+                    to={route.path}
+                  >
+                    {route.name}
+                  </Link>
+                );
+              } else return null;
+            })}
+            <button onClick={logout}>Log out</button>
+          </>
+        ) : (
+          <>
+            <Link className="menu-button" to="/login">
+              Přihlásit se
+            </Link>
+            <Link className="menu-button" to="/register">
+              Zaregistrovat se
+            </Link>
+          </>
+        )}
+      </nav>
+    </>
   );
 }
 
